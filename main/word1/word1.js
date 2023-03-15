dcc159a4ea58c9a8772ed92d7ee0c1a00c3b989d
ellin45 <119480528+ellin45@users.noreply.github.com>
2023-03-16 00:07:04 +0900

    commit

---
+++ main/word1/word1.js
@@ -0,0 +1,25 @@
+const button1 = document.querySelector('#iffy');
+
+const box1 = document.querySelector('#box');
+
+button1.addEventListener('click', function() {
+  box.classList.toggle('hide');
+});
+
+const button2 = document.querySelector('#know');
+
+const box2 = document.querySelector('#box');
+
+button2.addEventListener('click', function() {
+  box.classList.toggle('hide');
+});
+
+const button3 = document.querySelector('#idk');
+
+const box3 = document.querySelector('#box');
+
+button3.addEventListener('click', function() {
+  box.classList.toggle('hide');
+});
+
+
---
+++ main/word2/word2.js
@@ -0,0 +1,10 @@
+
+
+
+// const { translate } = require('bing-translate-api');
+
+// translate('한글', null, 'en').then(res => {
+//   console.log(res.translation);
+// }).catch(err => {
+//   console.error(err);
+// });
